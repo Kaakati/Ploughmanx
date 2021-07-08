7f898dd017308ef7bc9c1934f455870a700cdf8d
Mohamad Kaakati <m@kaakati.me>
2021-07-08 12:24:48 +0300

    Updated Documentation

--- Package.swift
+++ Package.swift
@@ -2,7 +2,7 @@ import PackageDescription
 
 
 let package = Package(
-  name: "Ploughmanxx",
+  name: "Ploughmanx",
   dependencies: [
     .Package(url: "https://github.com/kylef/PathKit.git", majorVersion: 0),
     .Package(url: "https://github.com/kylef/Commander.git", majorVersion: 0),
